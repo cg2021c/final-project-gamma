792d64cf80ace306843042a356ff7811c78a0327
GianEgaa <gianega78@gmail.com>
2021-12-14 00:51:42 +0700

    Update dinding

--- ground.js
+++ ground.js
@@ -7,7 +7,7 @@ document.body.appendChild( renderer.domElement );
 
 //camera
 camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
-camera.position.set( 50, 20, 0 );
+camera.position.set( 0, 20, 50 );
 camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
 
 // texture
@@ -42,16 +42,42 @@ var material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-//wall
-var loader = new THREE.GLTFLoader();
-loader.load('room.gltf', function(gltf){
-  room = gltf.scene;
-  scene.add(gltf.scene);
-});
+//material dinding
+const wallTexture = new THREE.TextureLoader().load('models/wall-texture.jpg');
+const wallMaterial = new THREE.MeshPhongMaterial({map: wallTexture});
 
-//controls
-// const controls = new PointerLockControls(camera, renderer.domElement);
-// var clock = new THREE.Clock();
+var geometry1 = new THREE.BoxGeometry( 20, 5, 0.2 );
+var wallz = new THREE.Mesh( geometry1, wallMaterial );
+wallz.position.set(0,2.5,7);
+scene.add( wallz );
+wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
+wallTexture.repeat.set(400,400);
+
+var geometry2 = new THREE.BoxGeometry( 20, 5, 0.2 );
+var wallz1 = new THREE.Mesh( geometry2, material );
+wallz1.position.set(0,2.5,-7);
+scene.add( wallz1 );
+
+var geometry3 = new THREE.BoxGeometry( 0.2, 5, 14.2);
+var wallx = new THREE.Mesh( geometry3, material );
+wallx.position.set(10,2.5,0);
+scene.add( wallx );
+
+var geometry4 = new THREE.BoxGeometry( 0.2, 5, 14.2 );
+var wallx1 = new THREE.Mesh( geometry4, material );
+wallx1.position.set(-10,2.5,0);
+scene.add( wallx1 );
+
+// //wall
+// var loader = new THREE.GLTFLoader();
+// loader.load('/models/room/room.gltf', function(gltf){
+//   room = gltf.scene;
+//   scene.add(gltf.scene);
+  
+// });
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.update();
 
 function drawScene(){
       renderer.render( scene, camera );
