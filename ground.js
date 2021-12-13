9b8f2da066a2d43566f0ff321740af4c429e6f12
GianEgaa <gianega78@gmail.com>
2021-12-13 21:08:04 +0700

    wall update
    menambahkan wall dan memperbaiki lighting

--- ground.js
+++ ground.js
@@ -1,32 +1,61 @@
+canvas = document.getElementById("mainCanvas");
 
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
+var scene = new THREE.Scene();
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 //camera
 camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 100 );
-camera.position.set( - 5, 3, 10 );
-camera.lookAt( new THREE.Vector3( 0, 2, 0 ) );
+camera.position.set( 50, 20, 0 );
+camera.lookAt( new THREE.Vector3( 0, 0, 0 ) );
+
+// texture
+const texture = new THREE.TextureLoader().load( 'floor.png' );
+const graniteMaterial = new THREE.MeshBasicMaterial({map: texture});
 
 //ground
-const mesh = new THREE.Mesh(new THREE.PlaneGeometry(200,200), new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false }))
+var mesh = new THREE.Mesh(new THREE.PlaneGeometry(200,200), graniteMaterial)
 mesh.rotation.x = - Math.PI /2;
 scene.add(mesh);
+texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(400,400);
 
 //grid
-const grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
+var grid = new THREE.GridHelper( 200, 40, 0x000000, 0x000000 );
 grid.material.opacity = 0.2;
 grid.material.transparent = true;
 scene.add( grid );
 
 //light
-const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-hemiLight.position.set( 0, 20, 0 );
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.5 );
+hemiLight.position.set( 0, 10, 0 );
 scene.add( hemiLight );
 
-const dirLight = new THREE.DirectionalLight( 0xffffff );
-dirLight.position.set( 0, 20, 10 );
+var dirLight = new THREE.DirectionalLight( 0xD9EBFA, 0.8 );
+dirLight.position.set( 0, 10, 10 );
 scene.add( dirLight );
 
-renderer.render( scene, camera );
+// cube
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var material = new THREE.MeshPhongMaterial( {color: 0xffffff} );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+//wall
+var loader = new THREE.GLTFLoader();
+loader.load('room.gltf', function(gltf){
+  room = gltf.scene;
+  scene.add(gltf.scene);
+});
+
+//controls
+// const controls = new PointerLockControls(camera, renderer.domElement);
+// var clock = new THREE.Clock();
+
+function drawScene(){
+      renderer.render( scene, camera );
+      requestAnimationFrame(drawScene);
+};
+
+drawScene();
